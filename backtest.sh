96a74383230ad637175f9b4b6c021b770015dde5
stealthelf9 <waldmannariel@gmail.com>
2026-03-04 00:47:57 +0200

    Add KeyboardInterrupt Checkpoint Saving logic

---
+++ backtest.sh
@@ -0,0 +1,13 @@
+python python/train_and_backtest.py \
+    --mode backtest \
+    --dataset equs \
+    --model-name "S&P_1min_V1" \
+    --start-date 2025-01-01 \
+    --end-date 2026-02-28 \
+    --strategies ai \
+    --timeframe 1m \
+    --capital 10000 \
+    --position-size 0.05 \
+    --commission 0.005 \
+    --slippage 0.005 \
+    --symbols ALL
